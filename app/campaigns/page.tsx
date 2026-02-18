ca3411736a9c2cae37561902eed8ce7f9c4313e9
Your Name <you@example.com>
2026-02-17 22:33:47 -0600

    Update page.tsx

--- app/campaigns/page.tsx
+++ app/campaigns/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import CampaignCard from "@/components/CampaignCard";
 import { campaigns } from "@/lib/data";
 import { getTrendingCampaigns } from "@/lib/campaignUtils";
